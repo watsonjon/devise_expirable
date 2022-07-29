7b7ef793d6b6363d289c71dceb827a9d0456a28d
Jon Dalberg <jon@releasehub.com>
2022-07-29 09:38:33 -0500

    ensure field is present

--- lib/devise_expirable/hooks/expirable.rb
+++ lib/devise_expirable/hooks/expirable.rb
@@ -7,7 +7,7 @@ Warden::Manager.after_set_user do |record, warden, options|
 
   if warden.authenticated?(scope)
     proxy = Devise::Hooks::Proxy.new(warden)
-    if record.respond_to?(:expired?) && record.expired?(record.current_sign_in_at)
+    if record.respond_to?(:expired?) && record.current_sign_in_at && record.expired?(record.current_sign_in_at)
       Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
     end
   end
